cf71d84a08242be6894df1d723d3ea72e182bdbf
Krishna Kant Singh <62882986+devkkxingh@users.noreply.github.com>
2026-02-04 22:39:36 +0530

    sanity

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Experience from './components/Experience';
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
-    <Experience />
+    <>
+      <Experience />
+      <Analytics />
+    </>
   );
 }
 
